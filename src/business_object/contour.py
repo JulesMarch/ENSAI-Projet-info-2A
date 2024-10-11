1bde1fa0359dfd73b3eb9acaa425df4f6670cafc
g.freydier <gaspard.freydier@gmail.com>
2024-10-11 14:45:05 +0200

    ajout méthode comptage_inters

--- src/business_object/contour.py
+++ src/business_object/contour.py
@@ -5,5 +5,9 @@ class Contour:
     def __init__(self, L: list(Segment)):
         self.lst_segments = L
 
-    def comptage_inters(self, D: Segment) -> int:
-        pass
+    def comptage_inters(self, DD: Segment) -> int:
+        nb_inters = 0
+        for seg in self.lst_segments:
+            if DD.detec_inters(seg):
+                nb_inters += 1
+        return nb_inters
