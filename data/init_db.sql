25bd1b5e62c843f0034a9058e1028d45a33f4f42
Raphael Mangatal <mangatalraphael@gmail.com>
2024-10-20 15:28:48 +0200

    Ajout des régions dans la bdd

--- data/init_db.sql
+++ data/init_db.sql
@@ -1,53 +1,19 @@
 DROP SCHEMA IF EXISTS projet CASCADE;
+
 CREATE SCHEMA projet;
 
 --------------------------------------------------------------
--- Point
+-- Zone
 --------------------------------------------------------------
 
-DROP TABLE IF EXISTS projet.point CASCADE ;
-CREATE TABLE projet.point (
-    id_point serial PRIMARY KEY,
-    x int,
-    y int
-);
+DROP TABLE IF EXISTS projet.zone_geo CASCADE ;
 
-
---------------------------------------------------------------
--- Table d'association entre polygone et point
---------------------------------------------------------------
-
-DROP TABLE IF EXISTS projet.association_polygone_point;
-
-CREATE table projet.association_polygone_point (
-    id_point integer references projet.point(id_point),
-    id_polygone integer references projet.polygone(id_polygone),
-    ordre serial
-);
-
-
---------------------------------------------------------------
--- Types de Polygone
---------------------------------------------------------------
-
-DROP TABLE IF EXISTS projet.polygone CASCADE ;
-
-CREATE TABLE projet.polygone (
-    id_polygone sequence PRIMARY KEY
-);
-
-
---------------------------------------------------------------
--- Table d'association entre composante connexe et polygone
---------------------------------------------------------------
-
-DROP TABLE IF EXISTS projet.association_connexe_polygone CASCADE;
-
-CREATE TABLE projet.association_connexe_polygone (
-	id_polygone integer REFERENCES tp.polygone(id_polygone),
-    id_comp_connexe integer REFERENCES tp.comp_connexe(id_comp_connexe),
-    ordre sequence
-    creux boolean
+CREATE TABLE projet.zone_geo (
+    id_zone serial PRIMARY KEY,
+    nom text,
+    niveau text,
+    code_insee text,
+    niveau_supperieur text
 );
 
 
@@ -58,9 +24,69 @@ CREATE TABLE projet.association_connexe_polygone (
 DROP TABLE IF EXISTS projet.comp_connexe CASCADE;
 
 CREATE TABLE projet.comp_connexe (
-    id_comp_connexe integer PRIMARY KEY,
-    id_attack integer REFERENCES tp.attack(id_attack) ON DELETE CASCADE,
-    level integer,
-    PRIMARY KEY (id_pokemon, id_attack)
+    id_comp_connexe serial PRIMARY KEY
+);
+
+
+--------------------------------------------------------------
+-- Polygone
+--------------------------------------------------------------
+
+DROP TABLE IF EXISTS projet.polygone CASCADE ;
+
+CREATE TABLE projet.polygone (
+    id_polygone serial PRIMARY KEY
+);
+
+
+--------------------------------------------------------------
+-- Point
+--------------------------------------------------------------
+
+DROP TABLE IF EXISTS projet.point CASCADE ;
+
+CREATE TABLE projet.point (
+    id_point serial PRIMARY KEY,
+    x int,
+    y int
+);
+
+
+--------------------------------------------------------------
+-- Association Zone et Composante connexe
+--------------------------------------------------------------
+
+DROP TABLE IF EXISTS projet.asso_zone_comp_co CASCADE ;
+
+CREATE TABLE projet.asso_zone_comp_co (
+    id_zone serial references projet.zone_geo,
+    id_comp_connexe serial references projet.comp_connexe
+);
+
+
+--------------------------------------------------------------
+-- Association Composante connexe et Polygone
+--------------------------------------------------------------
+
+DROP TABLE IF EXISTS projet.association_connexe_polygone CASCADE;
+
+CREATE TABLE projet.association_connexe_polygone (
+	id_polygone serial references projet.polygone,
+    id_comp_connexe serial references projet.comp_connexe,
+    ordre serial,
+    creux boolean
+);
+
+
+--------------------------------------------------------------
+-- Association Polygone et Point
+--------------------------------------------------------------
+
+DROP TABLE IF EXISTS projet.association_polygone_point;
+
+CREATE table projet.association_polygone_point (
+    id_point serial references projet.point,
+    id_polygone serial references projet.polygone,
+    ordre serial
 );
 
--- data/pop_db.py
+++ data/pop_db.py
@@ -1,5 +1,5 @@
 import fiona
-
+from src.dao.db_connection import DBConnection
 
 with fiona.open(
     "//filer-eleves2/id2505/Cours2A/" +
@@ -9,5 +9,22 @@ with fiona.open(
     "ADE_3-2_SHP_WGS84G_FRA-ED2024-09-18/REGION.shp",
     'r'
 ) as shapefile:
+    i = 0
+    for element in shapefile:
+        i += 1
+        properties = element["properties"]
+        geometry = element["geometry"]
 
-    schema = shapefile.schema
+        with DBConnection().connection as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    "INSERT INTO projet.zone_geo (id_zone, nom, niveau,     "
+                    " code_insee, niveau_supperieur) VALUES                 "
+                    "(%(id_zone)s, %(nom)s, 'Région',                       "
+                    " %(code_insee)s, 'blabla')                             ",
+                    {
+                        "id_zone": i,
+                        "nom": properties["NOM"],
+                        "code_insee": properties["INSEE_REG"],
+                    },
+                )
