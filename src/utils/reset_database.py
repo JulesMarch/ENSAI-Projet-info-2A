2b72b93dfe704b5f8e1a73678c094e24eee76572
g.freydier <gaspard.freydier@gmail.com>
2024-10-22 14:42:33 +0200

    bon reset

--- src/utils/reset_database.py
+++ src/utils/reset_database.py
@@ -13,17 +13,16 @@ class ResetDatabase(metaclass=Singleton):
         init_db = open("data/init_db.sql", encoding="utf-8")
         init_db_as_string = init_db.read()
 
-        pop_db = open("data/pop_db.py", encoding="utf-8")
-        pop_db_as_string = pop_db.read()
-
         try:
             with DBConnection().connection as connection:
                 with connection.cursor() as cursor:
                     cursor.execute(init_db_as_string)
-                    cursor.execute(pop_db_as_string)
         except Exception as e:
             print(e)
             raise
+
+        with open("data/pop_db.py") as f:
+            exec(f.read())
 
         print("Ré-initialisation de la base de données - Terminée")
 
